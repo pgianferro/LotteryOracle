7cb258232acbbc086dcf0dd5a2aaf249b8f57eff
pgianferro <pablogianferro@gmail.com>
2025-07-02 11:18:35 -0300

    upd/connect_private_net/pgianferro

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
+const { vars } = require("hardhat/config");
+require("dotenv").config();
+
+const INFURA_NODO = process.env.NODO; //ESCRIBIR EN LA BLOCKCHAIN
+const SEPOLIA_PRIVATE_KEY = process.env.PRKEY; //EL QUE PAGA EL GAS
+const ETHERSCAN_API_KEY = process.env.ETHSCAN_KEY; //VERIFICAR EL CONTRATO
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: INFURA_NODO,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
